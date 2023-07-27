239feb030ce302ca3c58d894a931974ea5d397ed
Elliot <ellthrowaway2@icloud.com>
2023-07-27 12:31:54 -0400

    deep space search successfully calls api, debugging the null/empty array api response on frontend still

--- galaxy-gaze/src/components/Search.jsx
+++ galaxy-gaze/src/components/Search.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
 import axios from 'axios'
-import { BASE_URL, GEOCODE_SEARCH } from "../globals"
+import { BASE_URL, GEOCODE_SEARCH, DEEP_SPACE_SEARCH_URL } from "../globals"
 
 const SearchComponent = () => {
     const [searchQuery, setSearchQuery] = useState("")
     const [userCoords, setUserCoords] = useState([])
+    const [searchResults, setSearchResults] = useState([])
 
     // user types in city, state to search for deep space object in their loc
     const handleChange = (event) => {
@@ -13,7 +14,25 @@ const SearchComponent = () => {
         setSearchQuery(query)
     }
 
-    // user submits search query
+    //////////////////////////////////////////////
+
+    // user submits search query for deep space object:
+
+    const handleDeepSpaceSearch = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await axios.get(`http://localhost:8000/search-deep-space/?term=${searchQuery}`) // testing local api endpoint for dev
+            // const response = await axios.get(`${DEEP_SPACE_SEARCH_URL}${searchQuery}`) // actual astro api endpoint
+            setSearchResults(response.data)
+            console.log(searchResults)
+        } catch (error) {
+            console.error("Error searching deep space:", error)
+        }
+    }
+
+
+    //////////////////////////////////////////////
+    // user submits search query to find bodies in their location
     // pass to geocode api to convert to coords
         const handleSearch = async (event) => {
             event.preventDefault()
@@ -39,7 +58,7 @@ const SearchComponent = () => {
 
         return (
             <div className='search'>
-                <form onSubmit={handleSearch}>
+                <form onSubmit={handleDeepSpaceSearch}>
                     <input
                         type='text'
                         value={searchQuery}
@@ -49,12 +68,24 @@ const SearchComponent = () => {
                     <button type='submit' className='submit-search'> Search </button>
                 </form>
                 <div className='results'>
-                    <ul className='search-results'>
+                    {/* <ul className='search-results'> */}
                         {/* {userCoords.map((result) => (
                             <li key={result.id}>{result.name}</li>
                         ))} */}
-                        <li className="your-coords">The coordinates for that location are {userCoords}</li>
-                    </ul>
+                        {/* <li className="your-coords">The coordinates for that location are {userCoords}</li> */}
+                    {/* </ul> */}
+                    {searchResults ? (
+                        searchResults.map((result, index) => (
+                            <div key={index}>
+                            <h4>{result.object_name}</h4>
+                            <p>Type of object: {result.object_type}</p>
+                            <p>Sub-type: {result.object_sub_type}</p>
+                            <p>Ojbect position: {result.object_position}</p>
+                        </div>
+                        ))
+                    ) : (
+                        <p> Loading search results... </p>
+                    )}
                 </div>
             </div>
         )
--- galaxy-gaze/src/globals.js
+++ galaxy-gaze/src/globals.js
@@ -2,3 +2,4 @@
 export const BASE_URL = "https://api.astronomyapi.com/api/v2"
 export const BODY_POSITION_URL = "/bodies/positions"
 export const GEOCODE_SEARCH = "https://geocode.maps.co/search?q="
+export const DEEP_SPACE_SEARCH_URL = "https://api.astronomyapi.com/api/v2/search?term="
