e4cf7d51667c25f0f1590ec3dd6c919c075a5420
Elliot <ellthrowaway2@icloud.com>
2023-07-27 15:08:08 -0400

    debugging api data response on FE

--- galaxy-gaze/src/components/Search.jsx
+++ galaxy-gaze/src/components/Search.jsx
@@ -1,64 +1,104 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import axios from 'axios'
 import { BASE_URL, GEOCODE_SEARCH, DEEP_SPACE_SEARCH_URL } from "../globals"
 
 const SearchComponent = () => {
     const [searchQuery, setSearchQuery] = useState("")
-    const [userCoords, setUserCoords] = useState([])
+    // const [userCoords, setUserCoords] = useState([])
     const [searchResults, setSearchResults] = useState([])
 
+    // useRef to maniuplate DOM
+    // const ref = useRef(null)
+
+    
     // user types in city, state to search for deep space object in their loc
     const handleChange = (event) => {
         const query = event.target.value
         // save search query in state for later use
         setSearchQuery(query)
     }
-
+    
     //////////////////////////////////////////////
-
+    
     // user submits search query for deep space object:
-
-    const handleDeepSpaceSearch = async (event) => {
-        event.preventDefault()
-        try {
-            const response = await axios.get(`http://localhost:8000/search-deep-space/?term=${searchQuery}`) // testing local api endpoint for dev
-            // const response = await axios.get(`${DEEP_SPACE_SEARCH_URL}${searchQuery}`) // actual astro api endpoint
-            setSearchResults(response.data)
-            console.log(searchResults)
-        } catch (error) {
-            console.error("Error searching deep space:", error)
+    // useEffect to let api call run & return results
+    useEffect(() => {
+        let isMounted = true // track component mount status
+    
+    // function to get deep space search results:
+        const getDeepSpaceData = async (event) => {
+            event.preventDefault()
+            // disable search button while awaiting response to prevent dupe reqs
+            try {
+                const response = await axios.get(`http://localhost:8000/search-deep-space/?term=${searchQuery}`) // testing local api endpoint for dev
+                // const response = await axios.get(`${DEEP_SPACE_SEARCH_URL}${searchQuery}`) // actual astro api endpoint
+                if (isMounted) {
+                    setSearchResults(response)
+                }
+            } catch (error) {
+                console.error("Error searching deep space:", error)
+            }
+            // finally { re-enable search button }
         }
-    }
+    
+        const handleDeepSpaceSearch = async () => {
+            // event.preventDefault()
+            try {
+                await getDeepSpaceData(searchQuery)
+            } catch (error) {
+                console.error("Error searching deep space:", error)
+            }
+        }
+        // handleDeepSpaceSearch()
+
+        // console.log(searchResults)
+        // if (searchResults) {
+        //     console.log('results found')
+        //     ref.current
+        // } else {
+        //     console.log('no results')
+        //     ref.current
+        // }
+        return () => {
+            isMounted = false
+        }
+    }, [searchQuery])
 
 
     //////////////////////////////////////////////
     // user submits search query to find bodies in their location
     // pass to geocode api to convert to coords
-        const handleSearch = async (event) => {
-            event.preventDefault()
-            try {
-                // make api request passing searchQuery (address)
-                const response = await axios.get(`${GEOCODE_SEARCH}${searchQuery}`)
-                // return api response latitude
-                const geoData = response.data
-                const latitude = geoData[0].lat
-                // return api response longitude
-                const longitude = geoData[0].lon
-                console.log("latitude: ", latitude, "longitude: ", longitude)
-                const coordinates = `${latitude}, ${longitude}`
-                // store coords in state -> coords
-                console.log(coordinates)
-                setUserCoords(coordinates)
-            } catch (error) {
-                console.error("Error getting coordinates:", error)
-            }
+    //     const handleSearch = async (event) => {
+    //         event.preventDefault()
+    //         try {
+    //             // make api request passing searchQuery (address)
+    //             const response = await axios.get(`${GEOCODE_SEARCH}${searchQuery}`)
+    //             // return api response latitude
+    //             const geoData = response.data
+    //             const latitude = geoData[0].lat
+    //             // return api response longitude
+    //             const longitude = geoData[0].lon
+    //             console.log("latitude: ", latitude, "longitude: ", longitude)
+    //             const coordinates = `${latitude}, ${longitude}`
+    //             // store coords in state -> coords
+    //             console.log(coordinates)
+    //             setUserCoords(coordinates)
+    //         } catch (error) {
+    //             console.error("Error getting coordinates:", error)
+    //         }
 
-        // pass coords state to be sent in astro api call
+    //     // pass coords state to be sent in astro api call
+    // }
+
+    // handle form submission
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        handleDeepSpaceSearch()
     }
 
         return (
             <div className='search'>
-                <form onSubmit={handleDeepSpaceSearch}>
+                <form onSubmit={handleSubmit}>
                     <input
                         type='text'
                         value={searchQuery}
@@ -75,8 +115,8 @@ const SearchComponent = () => {
                         {/* <li className="your-coords">The coordinates for that location are {userCoords}</li> */}
                     {/* </ul> */}
                     {searchResults ? (
-                        searchResults.map((result, index) => (
-                            <div key={index}>
+                        searchResults.map((result) => (
+                            <div key={result.id}>
                             <h4>{result.object_name}</h4>
                             <p>Type of object: {result.object_type}</p>
                             <p>Sub-type: {result.object_sub_type}</p>
@@ -84,7 +124,7 @@ const SearchComponent = () => {
                         </div>
                         ))
                     ) : (
-                        <p> Loading search results... </p>
+                        <p> No results found </p>
                     )}
                 </div>
             </div>
