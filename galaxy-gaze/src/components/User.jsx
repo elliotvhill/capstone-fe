92191898304ba69d967800f9cbcb16d2d4c7ec6f
Elliot <ellthrowaway2@icloud.com>
2023-08-01 13:48:51 -0400

    account page shows please sign in if user not logged in

--- galaxy-gaze/src/components/User.jsx
+++ galaxy-gaze/src/components/User.jsx
@@ -1,48 +1,49 @@
-import React, { useState, useEffect, useContext } from 'react'
-import UserContext from '../UserContext'
-import axios from 'axios'
+import React, { useState, useEffect, useContext } from "react"
+import UserContext from "../UserContext"
+import axios from "axios"
 
 const User = () => {
     const { userInfo, setUserInfo } = useContext(UserContext)
-    const [userId, setUserId] = useState([])
 
     // get the userId of logged in user
-
     useEffect(() => {
-        axios.get('http://localhost:8000/api/get_user_id')
-            .then(response => {
-            setUserId(response.data.user_id)
+        axios
+            .get("http://localhost:8000/api/get_user_id")
+            .then((response) => {
+                setUserId(response.data.user_id)
             })
-            .catch(error => {
-            console.log(error)
-        })
+            .catch((error) => {
+                console.log(error)
+            })
     }, [])
 
+    console.log("User info:", userInfo)
     // display favorited/followed items associated w/ the id of the user
 
-    console.log('User info:', userInfo)
-    // console.log('Followed bodies:', followed_bodies)
-    console.log('User ID:', userId)
-
     return (
-        <div className="user">
-            {userInfo.userId ? <p>User ID: {userInfo.userId}</p> : <p>Loading...</p>}
-            <h2>Welcome, {`${userInfo.username}`}</h2>
-            <div className='followed-items'>
-                {userInfo.followed_bodies && userInfo.followed_bodies.length > 0 ? (
-                    userInfo.followed_bodies.map((body) => {
-                    return (
-                        <div key={body.id}>
-                        <div className='body'>
-                            {body.name}
-                        </div>
+        <div className='user'>
+            {userInfo.userId ? (
+                <>
+                    <p>User ID: {userInfo.userId}</p>
+                    <h2>Welcome, {`${userInfo.username}`}</h2>
+                    <div className='followed-items'>
+                        {userInfo.followed_bodies &&
+                        userInfo.followed_bodies.length > 0 ? (
+                            userInfo.followed_bodies.map((body) => {
+                                return (
+                                    <div key={body.id}>
+                                        <div className='body'>{body.name}</div>
+                                    </div>
+                                )
+                            })
+                        ) : (
+                            <p className='no-faves'>No favorited items</p>
+                        )}
                     </div>
-                        )
-                }) 
-                    ) : (
-                        <p className='no-faves'>No favorited items</p>
-                )}
-            </div>
+                </>
+            ) : (
+                <p> Please sign in.</p>
+            )}
         </div>
     )
 }
